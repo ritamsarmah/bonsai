9bbf1d798f08dc670841caf7ed00a7b4334c07ea
ritamsarmah <sarmah.ritam@gmail.com>
2018-11-07 19:51:24 -0800

    Add bootstrap navbar

--- public/js/test.js
+++ public/js/test.js
@@ -1,9 +1,5 @@
 // FOR DEMO PURPOSES ONLY
 
-document.onload = function () {
-    
-}
-
 backgroundColor = ["#B5EAD5", "white"];
 
 var percentage = 0.75
@@ -13,6 +9,25 @@ var myDoughnutChart = new Chart(ctx, {
     data: {
         datasets: [{
             data: [percentage, 1 - percentage],
+            backgroundColor: backgroundColor,
+        }],
+    },
+    options: {
+        cutoutPercentage: 80,
+        hover: { mode: null },
+        tooltips: {
+            enabled: false
+        }
+    }
+});
+
+var ctx = document.getElementById("progress-chart").getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            data: [3, 4, 2, 1, 4],
+            yAxisID: "Hours",
             backgroundColor: backgroundColor,
         }],
     },
